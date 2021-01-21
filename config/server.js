c911b8662b620ee4db0ab815a5b4ac624e816d81
akbar <akhan2023@yahoo.com>
2021-01-21 16:05:42 +0100

    f c

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f93fda4249adc9a6df809691503dae49'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dabebd2e-495d-46fb-950c-a308444465b4'
+};
